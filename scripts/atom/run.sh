3ef7bd42563776b8ecaf3ea721ebb8a76a74823e
kudomcho <kudomcho@amd.com>
2026-02-18 21:28:50 +0000

    fixed run.sh

--- scripts/atom/run.sh
+++ scripts/atom/run.sh
@@ -1,61 +1,46 @@
 #!/bin/bash
 ###############################################################################
-#
 # MIT License
-#
-# Copyright (c) Advanced Micro Devices, Inc.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-#################################################################################
-set -ex
+###############################################################################
 
-# # Preliminary setup
-# export HF_HUB_CACHE="/myworkspace"
-# MAD_MODEL_NAME=$(echo $MAD_MODEL_NAME | tr "/" "_")
+trap '' HUP
+set -e
+set -x
 
-# while [[ "$#" -gt 0 ]]; do
-#     case $1 in
-#         --model_repo) MODEL="$2"; shift ;;
-#         --config) CONFIG_ARG="$2"; shift ;;
-#         --benchmark) BENCHMARK_ARG="$2"; shift ;;
-#         *) echo "Unknown parameter passed: $1"; usage ;;
-#     esac
-#     shift
-# done
+# ---- Install deps (ONLY if you really must) ----
+# pip install -qqq lm-eval[api]
 
-# # By default run all benchmarks in configs/default.yaml; accept either CLI or env variable overrides
-# if [[ -z "$BENCHMARK" ]]; then
-#     BENCHMARK=${BENCHMARK_ARG:-"all"}
-# fi
-# if [[ -z "$CONFIG" ]]; then
-#     CONFIG=${CONFIG_ARG:-"configs/default.yaml"}
-# fi
+# ---- Clone benchmark repo ----
+if [[ ! -d bench_serving ]]; then
+  git clone https://github.com/kimbochen/bench_serving.git
+fi
 
-# install lm-eval for accuracy testing
-pip install -qqq lm-eval[api]
+# ---- Start server (detached) ----
+nohup python3 -m atom.entrypoints.openai_server \
+  --model openai/gpt-oss-120b \
+  -tp 8 \
+  --kv_cache_dtype fp8 \
+  --host 0.0.0.0 \
+  --port 8000 \
+  > server.log 2>&1 &
 
-git clone git clone https://github.com/kimbochen/bench_serving.git
-# # Run benchmark; use -u to make python prints unbuffered
-# python3 -u run_atom.py --config $CONFIG --model $MODEL --benchmark $BENCHMARK
-python3 -m atom.entrypoints.openai_server --model openai/gpt-oss-120b -tp 8 --kv_cache_dtype fp8 --host 0.0.0.0 --port 8000 
-# move the output csv to parent directory
-MODEL_NAME=$(basename $MODEL)
-OUTPUT_CSV="perf_${MODEL_NAME}.csv"
-mv $OUTPUT_CSV ../
+SERVER_PID=$!
+
+# ---- Wait for server readiness ----
+for i in {1..300}; do
+  if curl -sf http://127.0.0.1:8000/v1/models >/dev/null; then
+    break
+  fi
+  sleep 2
+done
+
+if ! curl -sf http://127.0.0.1:8000/v1/models >/dev/null; then
+  echo "[ERROR] Server failed to start"
+  exit 1
+fi
+
+# ---- Run your benchmark HERE ----
+# python bench_serving/benchmark_serving.py ...
+
+# ---- Cleanup ----
+kill "${SERVER_PID}" || true
